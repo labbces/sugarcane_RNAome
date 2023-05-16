2bd5138d53c46688c3c9c6029cea7afb4fbc35ad
felipevzps <felipe.vzps@gmail.com>
2023-05-16 18:41:51 -0300

    added Snakefile to run RNAplonc steps

---
+++ scripts/runRNAplonc/rnaplonc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#$ -q all.q
+#$ -cwd
+#$ -pe smp 1
+
+module load CD-HIT/4.8.1
+module load miniconda3
+conda activate /home/felipe.peres/.conda/envs/snakemake-tutorial/envs/snakemake_conekt
+
+snakemake -p -s Snakefile --resources load=5 --cluster "qsub -q all.q -V -cwd -pe smp {threads}" --jobs 5 --latency-wait 250
