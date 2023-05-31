e63f3285309a740dd3a371161ca53bed1a59f180
felipevzps <felipe.vzps@gmail.com>
2023-05-31 10:36:49 -0300

    fixed script to submit Snakefile

--- scripts/runSalmon/submit_Snakefile.sh
+++ scripts/runSalmon/submit_Snakefile.sh
@@ -10,4 +10,4 @@ module load miniconda3
 module load salmon/1.8.0
 conda activate /home/felipe.peres/.conda/envs/snakemake-tutorial/envs/snakemake_conekt
 
-snakemake -p -k --resources load=20 -s Snakefile --cluster "qsub -q all.q -V -cwd -l h={params.server} -pe smp {threads}" --jobs 20 --latency-wait 60
+snakemake -p -k --resources load=10 -s Snakefile --cluster "qsub -q all.q -V -cwd -l h={params.server} -pe smp {threads}" --jobs 10 --latency-wait 60
