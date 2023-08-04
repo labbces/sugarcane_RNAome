31cb706ccf3c2c7dea12117fe94f522001fb320d
Felipe Vaz Peres <fvperes@lovelace.ada.cenapad.unicamp.br>
2023-08-04 10:08:40 -0300

    updated submitCluster_par128.sh for putative_ncRNA_consensus.fa

--- scripts/runMMseqs2/submitCluster_par128.sh
+++ scripts/runMMseqs2/submitCluster_par128.sh
@@ -11,14 +11,13 @@ conda activate mmseqs2
 
 cd $PBS_O_WORKDIR
 
-#create MMseqs2 DB
-mmseqs createdb 48_genotypes_transcriptome.fasta DB
+mmseqs createdb putative_ncRNA_consensus.fa DB
 
 #DB - transcriptomes database
 #DB_clust - clusterized database
 #/work/fvperes - empty directory for temporary files
 
-#running without --split-memory-limit 40G (par128)
+#running without --split-memory-limit 40G
 
 mmseqs cluster --threads 128 -s 5.7 --cov-mode 2 --cluster-mode 2 -c 0.8 --min-seq-id 0.8 DB DB_clust /work/fvperes
 mmseqs createsubdb DB_clust DB DB_clust_rep
