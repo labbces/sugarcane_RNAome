4995ca22da2b9f3af8528b06cd65f1c745942947
felipevzps <felipe.vzps@gmail.com>
2023-08-28 10:42:21 -0300

    added limited threads to salmon quant rule

--- scripts/runSalmon/Snakefile.sh
+++ scripts/runSalmon/Snakefile.sh
@@ -10,4 +10,4 @@ module load miniconda3
 module load salmon/1.8.0
 conda activate /home/felipe.peres/.conda/envs/snakemake-tutorial/envs/snakemake_conekt
 
-snakemake -p -k --resources load=6 -s Snakefile --cluster "qsub -q all.q -V -cwd -l h={params.server} -pe smp {threads}" --jobs 6 --jobname "{rulename}.{jobid}"
+snakemake -p -k --resources load=6 -s Snakefile --rerun-incomplete --cluster "qsub -q all.q -V -cwd -pe smp {threads}" --jobs 6 --jobname "{rulename}.{jobid}"
