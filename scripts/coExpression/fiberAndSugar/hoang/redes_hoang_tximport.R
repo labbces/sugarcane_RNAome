d00cf356652c37d47eb60f13ee601f7cf35f27ba
felipevzps <felipe.vzps@gmail.com>
2023-09-26 13:49:01 -0300

    calculating and plotting CV histogram

--- scripts/coExpression/fiberAndSugar/hoang/redes_hoang_tximport.R
+++ scripts/coExpression/fiberAndSugar/hoang/redes_hoang_tximport.R
@@ -13,7 +13,7 @@ samples <- read.table(file.path(HOME_DIR, "samples.txt"), header = TRUE)
 samples
 
 # Set quant.sf files 
-files <- file.path(HOME_DIR, "smallData", samples$run, "quant_non-coding.sf")
+files <- file.path(HOME_DIR, "smallData", samples$run, "quant.sf")
 all(file.exists(files))
 
 # Set tx2gene file (clusters from MMSeqs2)
@@ -29,60 +29,20 @@ txi <- tximport(files, type = "salmon", tx2gene = tx2gene)
 names(txi)
 head(txi$counts)
 
+# Calculate the Coefficient of Variation (CV) for each gene
+cv <- apply(txi$counts, 1, function(x) sd(x) / mean(x) * 100)
 
+# Add the CV as a new column to txi object
+txi$cv <- cv
+names(txi)
+print(txi$cv)
 
-# expression matrix in TPM
-raw <- read.table("hoang_merged_quant_counts.txt", head = T, row.names = 1)
+# Open a PNG device for saving the plot
+png(filename = "cv_histogram.png", width = 800, height = 600)
 
-#convertendo valores em inteiros
-raw <- round(raw)
+# Plot a histogram of the Coefficient of Variation (CV)
+hist(txi$cv, breaks = 20, main = "Coefficient of Variation Distribution",
+     xlab = "Coefficient of Variation (%)", ylab = "Frequency")
 
-row_data <- read.table("metadata.txt")
-colnames(raw) <- row_data$V2
-
-#> install.packages(c("HiClimR", "wesanderson", "WGCNA"))
-library(dplyr)
-library(tidyr)
-
-## make a raw PCA for quality control, in this part we don't want to aggregate replicates
-library(ggplot2)
-library(ggrepel)
-library(DESeq2)
-#library(WGCNA)
-library(WGCNA, lib.loc = "/Storage/data1/jorge.munoz/NRGSC.new/libraries")
-library(tibble)
-
-raw.good <- raw[goodGenes(t(raw)),]
-raw.vst <- vst(as.matrix(raw.good))
-
-raw.pca <- prcomp(x = t(raw.vst), scale = TRUE)
-var_explained <- raw.pca$sdev^2/sum(raw.pca$sdev^2)
-
-library(viridisLite)
-library(viridis)
-colors <- viridis::viridis(20) #20 cores
-
-pca_plot <- raw.pca$x %>% 
-  as.data.frame %>%
-  rownames_to_column("group") %>%
-  separate(group,c("genotype", "tissue"), sep = "_") %>%
-  unite(reps, c("genotype", "tissue"), remove = F)  %>% 
-  ggplot(aes(x=PC1,y=PC2, label = tissue, fill = genotype )) + 
-  geom_text(check_overlap = TRUE, vjust = 1.5) +
-  geom_point(aes(color= genotype, shape = tissue ), size = 4 ) + 
-  scale_color_manual(values = colors) +
-  theme_bw(base_size=20) + 
-  labs(x=paste0("PC1: ",round(var_explained[1]*100,1),"%"),
-       y=paste0("PC2: ",round(var_explained[2]*100,1),"%")) +
-  theme(legend.position="top",
-        text = element_text(family = "Times New Roman", size=20), 
-        panel.border = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.text.x=element_text(colour="black", angle = 8, vjust = 0.7, hjust=0.5),
-        axis.text.y=element_text(colour="black"),
-        axis.line = element_line(colour = "black"))
-
-# Salve o PCA como um arquivo PNG
-ggsave("hoang_pca_plot.png", pca_plot, width = 20, height = 16, units = "in", dpi = 300)
-
+# Close the PNG device to save the plot
+dev.off()
