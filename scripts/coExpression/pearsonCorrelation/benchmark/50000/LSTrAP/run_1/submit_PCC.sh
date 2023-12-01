7cbaedec4c27fe4632419d9139d396d3dc692078
felipevzps <felipe.vzps@gmail.com>
2023-11-30 22:23:42 -0200

    run_1 LSTrAP 50000 genes

---
+++ scripts/coExpression/pearsonCorrelation/benchmark/50000/LSTrAP/run_1/submit_PCC.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#$ -q all.q
+#$ -V
+#$ -cwd
+#$ -pe smp 1
+
+module load Python/3.7.2
+
+INFILE=`ls -1 *0.txt`
+NETWORK=${INFILE/.txt/_network.txt}
+MCL=${INFILE/.txt/_mcl_out.txt}
+
+/usr/bin/time -v python3 pcc.py $INFILE $NETWORK $MCL
