d7f67a01e8cfd55930ad03fd1844b52ad5550d31
felipevzps <felipe.vzps@gmail.com>
2024-01-10 06:03:47 -0300

    fixed genotype names, beautiful heatmap

--- scripts/coExpression/moduleHeatmap/Correr/CNC/make_Correr_heatmaps_for_all_modules.R
+++ scripts/coExpression/moduleHeatmap/Correr/CNC/make_Correr_heatmaps_for_all_modules.R
@@ -7,11 +7,12 @@ library(RColorBrewer)
 
 rm(list=ls())
 
-DIR = "/home/felipe/Documents/sugarcane_RNAome/scripts/coExpression/moduleHeatmap/Correr/CNC"
+DIR = "/home/felipe/Documents/exploring_Jorge_heatmaps/Correr/CNC/heatmap_per_module"
 setwd(DIR)
 
 # *** Read file for modules numbers (1,2,3,4,5 ...) *** 
 Nmods <- read.table("all_mods.txt", header = F)
+Nmods <- read.table("one_mods.txt", header = F)
 colnames(Nmods) <- "Mod No"
 
 # *** Read formated modules ***
@@ -23,53 +24,63 @@ modules$gene <- rownames(modules)
 # *** Read filtered VST matrix ***
 vst_path <- "Correr2020_counts_filters_VST_top20CV.txt"
 # Define first column as index
-vst <- read.table(vst_path, header = TRUE, row.names = 1) #encoding = "UTF-8", check.names = FALSE
+vst <- read.table(vst_path, header = TRUE, row.names = 1, check.names = FALSE) #encoding = "UTF-8", check.names = FALSE
+
+# Extract only the first name before the hyphen in row names
+colnames(vst) <- sub("^([^_]+)_.*", "\\1", colnames(vst))
+# This regular expression pattern looks for a lowercase letter followed by an uppercase letter and inserts a space between them
+colnames(vst) <- sub("([a-z])([A-Z])", "\\1 \\2", colnames(vst))
 
 # *** Read metadata ***
 metadata_path <-"infos_correr_metadata.tsv"
 metadata <- read.table(metadata_path, sep = "\t", header = T)
 
-# *** Define groups to plot (Condition, Genotype) ***
+# *** Define groups to plot (Groups, Genotypes) ***
 sample_table <- metadata
 
-# Condition
-sample_table$Condition <- sub(".*_(high|low)", "\\1", metadata$Run)
-# Sub hyphen for dot
-sample_table$Condition <- gsub("-", ".", sample_table$Condition)
-sample_table$Condition <- as.factor((sample_table$Condition))
+# Groups
+sample_table$Groups <- sub(".*_(high|low)", "\\1", metadata$Run)
+# Sub hyphen for space
+sample_table$Groups <- gsub("-", " ", sample_table$Groups)
+sample_table$Groups <- as.factor((sample_table$Groups))
 
-# Genotype
-sample_table$Genotype <- sub("^(.*?)_.*", "\\1", metadata$Run)
-sample_table$Genotype <- as.factor((sample_table$Genotype)) 
+# Genotypes
+sample_table$Genotypes <- sub("^(.*?)_.*", "\\1", metadata$Run)
+sample_table$Genotypes <- as.factor((sample_table$Genotypes)) 
+# This regular expression pattern looks for a lowercase letter followed by an uppercase letter and inserts a space between them
+sample_table$Genotypes <- sub("([a-z])([A-Z])", "\\1 \\2", sample_table$Genotypes)
 
-# Group (Genotype and Condition)
-sample_table$Group <- as.factor(paste(sample_table$Genotype, '_', sample_table$Condition, sep=''))
+# Group (Genotypes and Groups)
+sample_table$Group <- as.factor(paste(sample_table$Genotypes, ' ', sample_table$Groups, sep=''))
 
 annotation_col <- sample_table
 
 # Remove duplicates in annotation_col (metadata)
-unique_annotation_col <- distinct(annotation_col, Group, Genotype, Condition)
-anot <- select(unique_annotation_col, Genotype, Condition)
+unique_annotation_col <- distinct(annotation_col, Group, Genotypes, Groups)
+anot <- select(unique_annotation_col, Genotypes, Groups)
+
+# red (-) black (0) green (+)
+my_palette = colorRampPalette(c("red", "black", "green"))(n=1000)
 
 for (i in Nmods[,1]){
   names <- modules[modules$module_No == i,]
   df <- vst[names$gene,]
   
-  # *** Reorder the rows of 'anot' based on the order of 'Genotype' ***
-  merged_df <- merge(anot, data.frame(Genotype = colnames(df)), by = "Genotype", all.x = TRUE)
-  anot <- merged_df[order(match(colnames(df), merged_df$Genotype)), ]
-  # Update 'anot' rownames -> Genotype + Condition in names
+  # *** Reorder the rows of 'anot' based on the order of 'Genotypes' ***
+  merged_df <- merge(anot, data.frame(Genotypes = colnames(df)), by = "Genotypes", all.x = TRUE)
+  anot <- merged_df[order(match(colnames(df), merged_df$Genotypes)), ]
+  # Update 'anot' rownames -> Genotypes + Groups in names
   rownames(anot) <- colnames(df)
   colnames(df) <- colnames(vst)
   
   # heat map with mean values for column i.e media por modulo
-  png(paste0("hp_samples_module_", i, ".png", sep = ""), res = 300, width = 5.5*800, height = 5*2850)
+  png(paste0("module_", i, "_heatmap",".png", sep = ""), res = 300, width = 5.5*800, height = 5*2850)
   pheatmap(df,
-           main =paste0("Contrasting Genotypes (CNC Module ",i, ")", sep = "") ,
+           main =paste0("Genotypess contrasting in biomass production (CNC Module ",i, ")", sep = "") ,
            scale = "row",
            annotation_col = anot,
            show_rownames = T,
-           col = colorRampPalette(brewer.pal(11, "RdYlGn"))(299),
+           col = my_palette,
            cluster_cols = T,
            cluster_rows = T,
            cellwidth = NA,
--- scripts/coExpression/moduleHeatmap/Correr/CNC/make_Correr_heatmaps_for_modules.R
+++ scripts/coExpression/moduleHeatmap/Correr/CNC/make_Correr_heatmaps_for_modules.R
@@ -7,7 +7,7 @@ library(RColorBrewer)
 
 rm(list=ls())
 
-DIR = "/home/felipe/Documents/sugarcane_RNAome/scripts/coExpression/moduleHeatmap/Correr/CNC"
+DIR = "/home/felipe/Documents/exploring_Jorge_heatmaps/Correr/CNC/heatmap_per_module"
 setwd(DIR)
 
 # *** Read file for modules numbers (1,2,3,4,5 ...) *** 
@@ -22,7 +22,12 @@ colnames(modules) <- c("module_No")
 # *** Read filtered VST matrix ***
 vst_path <- "Correr2020_counts_filters_VST_top20CV.txt"
 # Define first column as index
-vst <- read.table(vst_path, header = TRUE, row.names = 1) #encoding = "UTF-8", check.names = FALSE
+vst <- read.table(vst_path, header = TRUE, row.names = 1, check.names = FALSE) #encoding = "UTF-8", check.names = FALSE
+
+# Extract only the first name before the hyphen in row names
+colnames(vst) <- sub("^([^_]+)_.*", "\\1", colnames(vst))
+# This regular expression pattern looks for a lowercase letter followed by an uppercase letter and inserts a space between them
+colnames(vst) <- sub("([a-z])([A-Z])", "\\1 \\2", colnames(vst))
 
 # *** Read metadata ***
 metadata_path <-"infos_correr_metadata.tsv"
@@ -33,27 +38,29 @@ for (i in Nmods$'Mod No'){
   assign(paste0("Module", i), modules %>% filter(module_No == i))
 }
 
-# *** Define groups to plot (Condition, Genotype) ***
+# *** Define groups to plot (Groups, Genotypes) ***
 sample_table <- metadata
 
-# Condition
-sample_table$Condition <- sub(".*_(high|low)", "\\1", metadata$Run)
-# Sub hyphen for dot
-sample_table$Condition <- gsub("-", ".", sample_table$Condition)
-sample_table$Condition <- as.factor((sample_table$Condition))
+# Groups
+sample_table$Groups <- sub(".*_(high|low)", "\\1", metadata$Run)
+# Sub hyphen for space
+sample_table$Groups <- gsub("-", " ", sample_table$Groups)
+sample_table$Groups <- as.factor((sample_table$Groups))
 
-# Genotype
-sample_table$Genotype <- sub("^(.*?)_.*", "\\1", metadata$Run)
-sample_table$Genotype <- as.factor((sample_table$Genotype)) 
+# Genotypes
+sample_table$Genotypes <- sub("^(.*?)_.*", "\\1", metadata$Run)
+sample_table$Genotypes <- as.factor((sample_table$Genotypes)) 
+# This regular expression pattern looks for a lowercase letter followed by an uppercase letter and inserts a space between them
+sample_table$Genotypes <- sub("([a-z])([A-Z])", "\\1 \\2", sample_table$Genotypes)
 
-# Group (Genotype and Condition)
-sample_table$Group <- as.factor(paste(sample_table$Genotype, '_', sample_table$Condition, sep=''))
+# Group (Genotypes and Groups)
+sample_table$Group <- as.factor(paste(sample_table$Genotypes, ' ', sample_table$Groups, sep=''))
 
 annotation_col <- sample_table
 
 # Remove duplicates in annotation_col (metadata)
-unique_annotation_col <- distinct(annotation_col, Group, Genotype, Condition)
-anot <- select(unique_annotation_col, Genotype, Condition)
+unique_annotation_col <- distinct(annotation_col, Group, Genotypes, Groups)
+anot <- select(unique_annotation_col, Genotypes, Groups)
 
 # *** Make vectors for each module ***
 for (i in Nmods$'Mod No'){
@@ -79,24 +86,26 @@ for (i in Nmods$'Mod No'){
 # *** Create a DataFrame from all modules list ***
 df <- do.call("rbind", dat_list)
 
-# *** Reorder the rows of 'anot' based on the order of 'Genotype' ***
-merged_df <- merge(anot, data.frame(Genotype = colnames(df)), by = "Genotype", all.x = TRUE)
+# *** Reorder the rows of 'anot' based on the order of 'Genotypes' ***
+merged_df <- merge(anot, data.frame(Genotypes = colnames(df)), by = "Genotypes", all.x = TRUE)
 
-anot <- merged_df[order(match(colnames(df), merged_df$Genotype)), ]
+anot <- merged_df[order(match(colnames(df), merged_df$Genotypes)), ]
 
-# Update 'anot' rownames -> Genotype + Condition in names
+# Update 'anot' rownames -> Genotypes + Groups in names
 rownames(anot) <- colnames(df)
 
+# red (-) black (0) green (+)
+my_palette = colorRampPalette(c("red", "black", "green"))(n=1000)
+
 # *** Plot heatmap with mean values for column (i.e. mean for each module)
-#png("meanOf30ModulesHeatmap.png", res = 300, width = 10*800, height = 10*2850)
-png("meanOf30ModulesHeatmap.png", res = 300, width = 800, height = 2850)
+png("meanOf30ModulesHeatmap.png", res = 300, width = 10*800, height = 10*2850)
 
 pheatmap(df,
-         main ="Contrasting Genotypes (CNC Modules)" ,
+         main ="Genotypess contrasting in biomass production (CNC Modules)" ,
          scale = "row",
          annotation_col = anot,
          show_rownames = T,
-         col = colorRampPalette(brewer.pal(11, "RdYlGn"))(299),
+         col = my_palette,
          cluster_cols = T,
          cluster_rows = T,
          cellwidth = NA,
