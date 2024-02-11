69ad7a3807f15fdbf041124026ef383286014c1a
felipevzps <felipe.vzps@gmail.com>
2024-02-11 20:21:05 -0200

    run HistogramByClassificationAndFunction.py

---
+++ scripts/coExpression/analysisCV/plotHistogram.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#$ -q all.q
+#$ -V
+#$ -cwd
+#$ -t 1
+#$ -pe smp 1
+
+module load miniconda3
+
+/usr/bin/time -v ./HistogramByClassificationAndFunction.py -cv Hoang2017_counts_filters_VST_CV_classified.txt -oc Hoang2017_filtered_CV_Classification.png -of Hoang2017_filtered_CV_Function.png -b 50
