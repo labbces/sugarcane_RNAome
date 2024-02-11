ebef600e7da6ae36843719c2cce84c1f654274c1
felipevzps <felipe.vzps@gmail.com>
2024-02-11 21:19:36 -0200

    add new threshold

--- scripts/coExpression/analysisCV/plotHistogram.sh
+++ scripts/coExpression/analysisCV/plotHistogram.sh
@@ -8,4 +8,4 @@
 
 module load miniconda3
 
-/usr/bin/time -v ./HistogramByClassificationAndFunction.py -cv Hoang2017_counts_filters_VST_CV_classified.txt -oc Hoang2017_filtered_CV_Classification.png -of Hoang2017_filtered_CV_Function.png -b 50 -t 1 1.5 1.8 2.0
+/usr/bin/time -v ./HistogramByClassificationAndFunction.py -cv Hoang2017_counts_filters_VST_CV_classified.txt -oc Hoang2017_filtered_CV_Classification.png -of Hoang2017_filtered_CV_Function.png -b 50 -t 1 1.3 1.5 1.8 2.0
