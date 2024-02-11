5a3dde570f0b49c000138595d354e2ec381f801a
felipevzps <felipe.vzps@gmail.com>
2024-02-11 21:11:06 -0200

    plot threshold lines

--- scripts/coExpression/analysisCV/HistogramByClassificationAndFunction.py
+++ scripts/coExpression/analysisCV/HistogramByClassificationAndFunction.py
@@ -10,6 +10,9 @@ parser.add_argument("-oc", dest="out_class", metavar="classification_CV.png", re
 parser.add_argument("-of", dest="out_function", metavar="function_CV.png", required=True, help="function histogram")
 parser.add_argument("-b", dest="bins", type=int, default=10, help="Number of bins for the histogram")
 
+# Additional arguments for threshold values
+parser.add_argument("-t", dest="thresholds", nargs="+", type=float, default=[], help="Threshold values for vertical lines")
+
 args = parser.parse_args()
 
 df = pd.read_csv(args.classification_file, sep='\t')
@@ -22,40 +25,73 @@ cv_max = df['CV'].max()
 classifications = df['Classification'].unique()
 for classification in classifications:
     plt.figure()
-    df[df['Classification'] == classification]['CV'].hist(bins=args.bins)
+    ax = df[df['Classification'] == classification]['CV'].hist(bins=args.bins)
     plt.title(f'Coefficient of variation - {classification}')
     plt.xlabel('Coefficient of variation')
     plt.ylabel('Frequency')
     plt.xlim(cv_min, cv_max)  # Definindo os limites do eixo X
+    
+    # Adicionar linhas verticais e contar itens acima do threshold
+    for i, threshold in enumerate(args.thresholds):
+        count = df[(df['Classification'] == classification) & (df['CV'] > threshold)]['CV'].count()
+        ax.axvline(x=threshold, color='red', linestyle='--')
+        plt.text(threshold, ax.get_ylim()[1] - (i + 1) * ((ax.get_ylim()[1] - ax.get_ylim()[0]) / 20), f'> {threshold}: {count}', color='red', rotation=0)
+    
     plt.savefig(f"{args.out_class}_{classification}.png")
 
 # Filter and plot histogram by each function
 functions = df['Function'].unique()
 for function in functions:
     plt.figure()
-    df[df['Function'] == function]['CV'].hist(bins=args.bins)
+    ax = df[df['Function'] == function]['CV'].hist(bins=args.bins)
     plt.title(f'Coefficient of variation - {function}')
     plt.xlabel('Coefficient of variation')
     plt.ylabel('Frequency')
     plt.xlim(cv_min, cv_max)  # Definindo os limites do eixo X
+    
+    # Adicionar linhas verticais e contar itens acima do threshold
+    for i, threshold in enumerate(args.thresholds):
+        count = df[(df['Function'] == function) & (df['CV'] > threshold)]['CV'].count()
+        ax.axvline(x=threshold, color='red', linestyle='--')
+        plt.text(threshold, ax.get_ylim()[1] - (i + 1) * ((ax.get_ylim()[1] - ax.get_ylim()[0]) / 20), f'> {threshold}: {count}', color='red', rotation=0)
+    
     plt.savefig(f"{args.out_function}_{function}.png")
 
 # Filter and plot histogram by classification
 plt.figure()
+ax = None
 for classification, group in df.groupby('Classification'):
-    group['CV'].hist(alpha=0.5, label=classification, bins=args.bins)
+    ax = group['CV'].hist(alpha=0.5, label=classification, bins=args.bins)
 plt.title('Coefficient of variation by classification')
 plt.xlabel('Coefficient of variation')
 plt.ylabel('Frequency')
 plt.legend()
+plt.legend(loc='center right')
+
+# Adicionar linhas verticais e contar itens acima do threshold
+for i, threshold in enumerate(args.thresholds):
+    count = df[df['CV'] > threshold]['CV'].count()
+    ax.axvline(x=threshold, color='red', linestyle='--')
+    plt.text(threshold, ax.get_ylim()[1] - (i + 1) * ((ax.get_ylim()[1] - ax.get_ylim()[0]) / 20), f'> {threshold}: {count}', color='red', rotation=0)
+
 plt.savefig(args.out_class)
 
 # Filter and plot histogram by function
 plt.figure()
+ax = None
 for function, group in df.groupby('Function'):
-    group['CV'].hist(alpha=0.5, label=function, bins=args.bins)
+    ax = group['CV'].hist(alpha=0.5, label=function, bins=args.bins)
 plt.title('Coefficient of variation by function')
 plt.xlabel('Coefficient of variation')
 plt.ylabel('Frequency')
 plt.legend()
+plt.legend(loc='center right')
+
+# Adicionar linhas verticais e contar itens acima do threshold
+for i, threshold in enumerate(args.thresholds):
+    count = df[df['CV'] > threshold]['CV'].count()
+    ax.axvline(x=threshold, color='red', linestyle='--')
+    plt.text(threshold, ax.get_ylim()[1] - (i + 1) * ((ax.get_ylim()[1] - ax.get_ylim()[0]) / 20), f'> {threshold}: {count}', color='red', rotation=0)
+
 plt.savefig(args.out_function)
+
