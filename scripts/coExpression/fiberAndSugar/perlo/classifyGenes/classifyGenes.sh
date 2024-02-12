6af24356257150661f95136ffdcb96771c992b1c
felipevzps <felipe.vzps@gmail.com>
2024-02-12 02:23:24 -0200

    add classify genes runs

---
+++ scripts/coExpression/fiberAndSugar/perlo/classifyGenes/classifyGenes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#$ -q all.q
+#$ -V
+#$ -cwd
+#$ -t 1
+#$ -pe smp 1
+
+module load miniconda3
+
+/usr/bin/time -v ./classifyGenesfromCVfile.py -c panTranscriptome_panRNAomeClassificationTable_hyphen_Class.tsv -cv Perlo2022_counts_filters_VST_CV.txt -o Perlo2022_counts_filters_VST_CV_classified.txt
