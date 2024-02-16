a3ab421447d81c8efa3a148f53c07928ac3055f3
felipevzps <felipe.vzps@gmail.com>
2024-02-16 08:02:08 -0200

    add -analyze y

--- scripts/coExpression/MCL/run_mcl.sh
+++ scripts/coExpression/MCL/run_mcl.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-
-# -t 1-12
+#$ -t 1-12
 #$ -cwd
 #$ -q all.q
 #$ -pe smp 1
-# -tc 20
+#$ -tc 20
 
+export m=$((SGE_TASK_ID-1))
 module load mcl/14-137
 
-#export m=$((SGE_TASK_ID-1))
-#LC_NUMERIC="en_US.UTF-8".
-#num=($(LC_NUMERIC="en_US.UTF-8". seq 1.3 0.5 6))
+LC_NUMERIC="en_US.UTF-8".
+num=($(LC_NUMERIC="en_US.UTF-8". seq 1.3 0.5 6))
 
-in=Perlo2022_mcl_out.txt
-out=${in/.txt}_I2.0.txt
+in=`ls -1 *_mcl.txt`
+out=pearson_mcl/
 
-/usr/bin/time -v mcl $in -I 2 -te 1 --abc -o $out
+mkdir $out
+mcl $in -I ${num[${m}]} -te 1 --abc -analyze y -o ${out}out.${num[${m}]} 
