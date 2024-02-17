93d675b3ef9491cc74f502882363a1837daf479e
felipevzps <felipe.vzps@gmail.com>
2024-02-17 05:02:15 -0200

    use more threads, analyze clusters

--- scripts/coExpression/MCL/run_mcl.sh
+++ scripts/coExpression/MCL/run_mcl.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#$ -t 1-12
+
+#$ -t 1-11
 #$ -cwd
 #$ -q all.q
-#$ -pe smp 1
-#$ -tc 20
+#$ -pe smp 5
+#$ -tc 5
 
-export m=$((SGE_TASK_ID-1))
 module load mcl/14-137
 
-LC_NUMERIC="en_US.UTF-8".
-num=($(LC_NUMERIC="en_US.UTF-8". seq 1.3 0.5 6))
+export m=$((SGE_TASK_ID-1))
+
+LC_NUMERIC="en_US.UTF-8"
+num=($(LC_NUMERIC="en_US.UTF-8" seq 1.3 0.5 6))
 
 in=`ls -1 *_mcl.txt`
 out=pearson_mcl/
