26cb09b4ac937337a75e463af6f5820cbf510a3a
felipevzps <felipe.vzps@gmail.com>
2024-02-22 00:59:56 -0300

    fix threads and array

--- scripts/coExpression/MCL/run_mcl.sh
+++ scripts/coExpression/MCL/run_mcl.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-#$ -t 1-11
 #$ -cwd
 #$ -q all.q
-#$ -pe smp 5
-#$ -tc 5
-
-module load mcl/14-137
+#$ -pe smp 10
+#$ -t 1-8
+#$ -tc 2
+#$ -l h=neotera
 
 export m=$((SGE_TASK_ID-1))
 
 LC_NUMERIC="en_US.UTF-8"
-num=($(LC_NUMERIC="en_US.UTF-8" seq 1.3 0.5 6))
+# [1.3 1.8 2.3 2.8 3.3 3.8 4.3 4.8 5.3 5.8]
+# num=($(LC_NUMERIC="en_US.UTF-8" seq 1.3 0.5 6))
+
+# [1.3 1.8 2.3 2.8 3.3 3.8 4.3 4.8]
+num=($(LC_NUMERIC="en_US.UTF-8" seq 1.3 0.5 5))
 
 in=`ls -1 *_mcl.txt`
 out=pearson_mcl/
 
-mkdir $out
+module load mcl/14-137
 
-/usr/bin/time -v mcl $in -I ${num[${m}]} -te 1 --abc -analyze y -o ${out}out.${num[${m}]} 
+mkdir $out
+/usr/bin/time -v mcl $in -I ${num[${m}]} -te $NSLOTS --abc -analyze y -o ${out}out.${num[${m}]} 
