6c30dfb1a65da1266f39a3799291e532cafae415
felipevzps <felipe.vzps@gmail.com>
2024-03-08 19:49:55 -0300

    fix filtering

--- scripts/coExpression/MCL/findsCliquesInGraph.R
+++ scripts/coExpression/MCL/findsCliquesInGraph.R
@@ -12,22 +12,21 @@ data <- read.table("../fiberAndSugar/correr/MCL/CNC/clusterSimilarity/Correr_clu
 data <- read.table("../fiberAndSugar/hoang/MCL/CNC/clusterSimilarity/Hoang_clusteringSimilarities.tsv", header = TRUE, sep = "\t")
 data <- read.table("../fiberAndSugar/perlo/MCL/CNC/clusterSimilarity/Perlo_clusteringSimilarities.tsv", header = TRUE, sep = "\t")
 
-data1<-data[which(data$Jaccard>=0.5),]
-data2<-data[which(data$Overlap>=0.5),]
 # *** Jaccard coefficient ***
+# Filter Jaccard >= 0.5
+jaccard_filtered <- data[which(data$Jaccard>=0.5),]
 
 # create Jaccard graph
-graph_jaccard <- graph_from_data_frame(data1, directed = FALSE, vertices = NULL)
+graph_jaccard <- graph_from_data_frame(jaccard_filtered, directed = FALSE, vertices = NULL)
 
 # define edge weight as Jaccard coefficient
-E(graph_jaccard)$weight <- data1$Jaccard
+E(graph_jaccard)$weight <- jaccard_filtered$Jaccard
 
 # print graph
 print(graph_jaccard)
 
 # find cliques
 jaccard_cliques <- weighted_cliques(graph_jaccard, vertex.weights = NULL, min.weight = 0.5, maximal = TRUE)
-?weighted_cliques
 jaccard_cliques
 
 # find largest cliques
@@ -38,12 +37,14 @@ jaccard_largest_cliques
 weighted_clique_num(graph_jaccard)
 
 # *** Overlap coefficient ***
+# Filter Overlap >= 0.5
+overlap_filtered <- data[which(data$Overlap>=0.5),]
 
 # # create Overlap graph
-graph_overlap <- graph_from_data_frame(data2, directed = FALSE, vertices = NULL)
+graph_overlap <- graph_from_data_frame(overlap_filtered, directed = FALSE, vertices = NULL)
 
 # define edge weight as Overlap coefficient
-E(graph_overlap)$weight <- data2$Overlap
+E(graph_overlap)$weight <- overlap_filtered$Overlap
 
 # print graph
 print(graph_overlap)
@@ -82,4 +83,5 @@ save_cliques(jaccard_largest_cliques, "largest_cliques_jaccard.txt")
 # saving overlap cliques
 save_cliques(overlap_cliques, "cliques_overlap.txt")
 save_cliques(overlap_largest_cliques, "largest_cliques_overlap.txt")
+
 plot(data)
