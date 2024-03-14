52ec3f8d004f2f530712aaabdfd31c251c9628c9
felipevzps <felipe.vzps@gmail.com>
2024-03-14 13:15:59 -0300

    add annotation column

--- scripts/coExpression/moduleHeatmap/Perlo/CNC/make_Perlo_heatmaps_for_modules.R
+++ scripts/coExpression/moduleHeatmap/Perlo/CNC/make_Perlo_heatmaps_for_modules.R
@@ -10,91 +10,67 @@ rm(list=ls())
 DIR = "/home/felipe/Documents/sugarcane_RNAome/scripts/coExpression/moduleHeatmap/Perlo/CNC"
 setwd(DIR)
 
-# *** Read file for modules numbers (1,2,3,4,5 ...) *** 
-Nmods <- read.table("all_mods.txt", header = F)
-#Nmods <- read.table("one_mods.txt", header = F)
+Nmods <- read.table("all_mods.txt", header = F)                                    # read file with modules numbers (1,2,3,4,5 ...) 
 colnames(Nmods) <- "Mod No"
 
-# *** Read formated modules ***
-# formated cliques
-modules_path <- "Perlo2022_counts_filters_VST_topCV_mcl_formated_cliques.csv"
+modules_path <- "Perlo2022_counts_filters_VST_topCV_mcl_formated_cliques.csv"      # read formated modules (cliques)
 
-# TODO: Check duplicate genes in first column
-#modules <- read.table(modules_path, row.names = 1, header = F)
-modules <- read.table(modules_path, header = F)
-# Verificar e remover linhas duplicadas
+modules <- read.table(modules_path, header = F)                                    # remove duplicates from the first column
 modules <- modules[!duplicated(modules$V1), ]
-# Atribuir os valores da primeira coluna como nomes de linha
-row.names(modules) <- modules$V1
-# Remover a primeira coluna dos dados
-modules <- modules[, -1]
-modules <- modules[, c(2,1)]
-# end TODO
 
-#colnames(modules) <- c("module_No")
+row.names(modules) <- modules$V1                                                   # set rownames as first column
+modules <- modules[, -1]                                                           # remove first column
+modules <- modules[, c(2,1)]                                                       # reorder columns
+
 colnames(modules) <- c("module_No", "classification")
-
 modules$gene <- rownames(modules)
 
-# *** Read filtered VST matrix ***
-vst_path <- "Perlo2022_counts_filters_VST_CNC_CV_above0.6.txt"
-# Define first column as index
-vst <- read.table(vst_path, header = TRUE, row.names = 1, check.names = FALSE) #encoding = "UTF-8", check.names = FALSE
+vst_path <- "Perlo2022_counts_filters_VST_CNC_CV_above0.6.txt"                     # read filtered VST matrix
+vst <- read.table(vst_path, header = TRUE, row.names = 1, check.names = FALSE)     # define first column as index
 
-# Extract only the first name before the hyphen in row names
-#colnames(vst) <- sub("^([^_]+)_.*", "\\1", colnames(vst))
-# This regular expression pattern looks for a lowercase letter followed by an uppercase letter and inserts a space between them
-#colnames(vst) <- sub("([a-z])([A-Z])", "\\1 \\2", colnames(vst))
-
-# *** Read metadata ***
-metadata_path <-"infos_perlo_metadata.tsv"
+metadata_path <-"infos_perlo_metadata.tsv"                                         # read metadata 
 metadata <- read.table(metadata_path, sep = "\t", header = T)
 
-# *** Define groups to plot (Groups, Genotypes) ***
-sample_table <- metadata
+sample_table <- metadata                                                           # define groups to plot (Groups, Genotypes)
 
-sample_table$Groups <- sub("^.*?_(\\S+)_\\d+-weeks.*$", "\\1", metadata$Run)
-sample_table$Groups <- as.factor((sample_table$Groups))
+sample_table$Internode <- sub("^.*?_(\\S+)_\\d+-weeks.*$", "\\1", metadata$Run)    # find weeks in the columns
+sample_table$Internode <- as.factor((sample_table$Internode))
 
-sample_table$Replicate <- sub("^.*?_([^_]+_\\d+-weeks)_\\S+$", "\\1", metadata$Run)
+sample_table$Replicate <- sub("^.*?_(\\d+)-weeks_\\S+$", "\\1", metadata$Run)      # week number (19 or 37)
 sample_table$Replicate <- as.factor((sample_table$Replicate))
 
-sample_table$Genotypes <- sub("^.*_(\\S+)$", "\\1", metadata$Run)
+sample_table$Genotypes <- sub("^.*_(\\S+)$", "\\1", metadata$Run)                  # genotype (last name after underscore)
 sample_table$Genotypes <- as.factor((sample_table$Genotypes))
 
-# Group (Genotypes and Groups)
-#sample_table$Group <- as.factor(paste(sample_table$Genotypes, ' ', sample_table$Groups, ' ', sample_table$Sugar, sep=''))
-sample_table$Group <- as.factor(paste(sample_table$Replicate, sep=''))
+sample_table$Group <- as.factor(paste(sample_table$Replicate, sep=''))             # Group (Genotypes and Groups)
 
 annotation_col <- sample_table
 
-# Remove duplicates in annotation_col (metadata)
-unique_annotation_col <- distinct(annotation_col, Group, Genotypes, Groups, Replicate)
-anot <- select(unique_annotation_col, Genotypes, Group)
+unique_annotation_col <- distinct(annotation_col, Genotypes, Internode, Replicate) # Remove duplicates in annotation_col (metadata)
 
-# red (-) black (0) green (+)
-my_palette = colorRampPalette(c("red", "black", "green"))(n=1000)
+anot <- select(unique_annotation_col, Genotypes, Internode, Replicate)             # annotation columns
+
+my_palette = colorRampPalette(c("red", "black", "green"))(n=1000)                  # red (-) black (0) green (+)
 
 for (i in Nmods[,1]){
   names <- modules[modules$module_No == i,]
   df <- vst[names$gene,]
   
-  # *** Reorder the rows of 'anot' based on the order of 'Genotypes' ***
+  # Reorder the rows of 'anot' based on the order of 'Genotypes'
   merged_df <- merge(anot, data.frame(Genotypes = colnames(df)), by = "Genotypes", all.x = TRUE)
   anot <- merged_df[order(match(colnames(df), merged_df$Genotypes)), ]
+  
+  # Rename colnames(df)
+  colnames(df) <- gsub("Internode_([0-9]+)_([0-9]+).weeks_(.*)", "\\1_\\2_\\3", colnames(df))
+  colnames(df) <- gsub("Internode_Ex\\.5_37\\.weeks_(.*)", "Ex.5_37_\\1", colnames(df))
   # Force the use of "-" in the rownames instead of "."
-  colnames(df) <- gsub("Internode_", "", colnames(df))
-  
-  colnames(df) <- gsub("-weeks_", "_", colnames(df))
-  
   colnames(df) <- gsub("\\.", "-", colnames(df))
   
-  # Update 'anot' rownames -> Genotypes + Groups in names
+  # update 'anot' rownames -> Genotypes + Groups in names
   rownames(anot) <- colnames(df)
-  #colnames(df) <- colnames(vst)
-  
-  # heat map with mean values for column i.e media por modulo
-  png(paste0("module_", i, "_heatmap",".png", sep = ""), res = 300, width = 5*800, height = 5*2850) # Too big
+
+  # pheatmap with mean values for column (mean module expression) 
+  png(paste0("module_", i, "_heatmap",".png", sep = ""), res = 300, width = 5*1500, height = 5*2850) # Too big
   pheatmap(df,
            main =paste0("Genotypes contrasting in biomass production (CNC Module ",i, ")", sep = "") ,
            scale = "row",
