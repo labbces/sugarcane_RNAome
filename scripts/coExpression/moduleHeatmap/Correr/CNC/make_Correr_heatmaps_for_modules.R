ca2d3b86f6ff74aa92926402e661f0aa0889a057
felipevzps <felipe.vzps@gmail.com>
2024-03-19 18:40:37 -0300

    fixed heatmap, add row annotation

--- scripts/coExpression/moduleHeatmap/Correr/CNC/make_Correr_heatmaps_for_modules.R
+++ scripts/coExpression/moduleHeatmap/Correr/CNC/make_Correr_heatmaps_for_modules.R
@@ -14,23 +14,21 @@ setwd(DIR)
 Nmods <- read.table("all_mods.txt", header = F)                                    # read file with modules numbers (1,2,3,4,5 ...) 
 colnames(Nmods) <- "Mod No"
 
-modules_path <- "Correr2020_counts_filters_VST_topCV_mcl_formated_cliques.csv"     # read formated modules (cliques)
+modules_path <- "Correr2020_counts_filters_VST_topCV_mcl_formated_cliques.csv"     # filename for formated modules (cliques)
+classification_path <- "Correr2020_counts_filters_VST_CV_classified.txt"           # filename for annotation file (CV and Function)
 
-modules <- read.table(modules_path, header = F)                                    # remove duplicates from the first column
-modules <- modules[!duplicated(modules$V1), ]
+modules <- read.table(modules_path, header = F)                                    # read file with modules classification
+colnames(modules) <- c("Gene", "Membership", "module_No")                          # rename columns
 
-row.names(modules) <- modules$V1                                                   # set rownames as first column
-modules <- modules[, -1]                                                           # remove first column
-modules <- modules[, c(2,1)]                                                       # reorder columns
+classification_file <- read.table(classification_path, row.names = 1, header = T)  # read file with function and pantranscriptome classfication
 
-colnames(modules) <- c("module_No", "classification")
-modules$gene <- rownames(modules)
+merged_annotation <- merge(modules, classification_file, by.x = "Gene", by.y = "row.names", all.x = TRUE)
 
 vst_path <- "Correr2020_counts_filters_VST_CNC_CV_above2.txt"                      # read filtered VST matrix
 vst <- read.table(vst_path, header = TRUE, row.names = 1, check.names = FALSE)     # define first column as index
 
-metadata_path <-"infos_correr_metadata.tsv"                                        # read metadata 
-metadata <- read.table(metadata_path, sep = "\t", header = T)
+metadata_path <-"infos_correr_metadata.tsv"                                        # metadata file 
+metadata <- read.table(metadata_path, sep = "\t", header = T)                      # read metadata
 
 sample_table <- metadata                                                           # define groups to plot (sugar content, genotypes)
 
@@ -51,17 +49,24 @@ anot <- select(unique_annotation_col, Genotypes, Brix)                          
 my_palette = colorRampPalette(c("red", "black", "green"))(n=1000)                  # red (-) black (0) green (+)
 
 for (i in Nmods[,1]){
-  names <- modules[modules$module_No == i,]
-  df <- vst[names$gene,]
+  names <- merged_annotation[merged_annotation$module_No == 2,]
+  df <- vst[names$Gene,]
   
   # reorder the rows of 'anot' based on the order of 'Genotypes'
   merged_df <- merge(anot, data.frame(Genotypes = colnames(df)), by = "Genotypes", all.x = TRUE)
   anot <- merged_df[order(match(colnames(df), merged_df$Genotypes)), ]
+
+  # Create annotation row object
+  anot_row <- names[, c("Gene", "Membership", "Classification", "Function")]
+  rownames(anot_row) <- names$Gene
+  anot_row <- anot_row[, -1]                                                       # remove first column
+  colnames(anot_row) <- c("Membership", "Group", "Function")                       # rename row annotation columns  
   
   # force the use of "-" in the rownames instead of "."
   colnames(df) <- gsub("\\.", "-", colnames(df))
   
   # update 'anot' rownames -> genotypes + sugar content in names
+  colnames(df) <- anot$Genotypes
   rownames(anot) <- colnames(df)
   
   # pheatmap with mean values for column (mean condition expression) 
@@ -70,6 +75,7 @@ for (i in Nmods[,1]){
            main =paste0("Genotypes contrasting in biomass production (CNC Module ",i, ")", sep = "") ,
            scale = "row",
            annotation_col = anot,
+           annotation_row = anot_row,
            show_rownames = T,
            col = my_palette,
            cluster_cols = T,
