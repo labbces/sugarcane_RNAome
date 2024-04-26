ac683f443b2ed3514dfda046a0ce41be03813e42
felipevzps <felipe.vzps@gmail.com>
2024-04-25 21:24:30 -0300

    fix and add comments

--- scripts/coExpression/networkMetrics/parallelBetweenness_CENAPAD.sh
+++ scripts/coExpression/networkMetrics/parallelBetweenness_CENAPAD.sh
@@ -6,6 +6,10 @@
 #PBS -e parallelBetweenness.sh.err
 #PBS -o parallelBetweenness.sh.out
 
+# testando na memlong e paralell:
+# -q paralela
+# -l nodes=4:ppn=128
+
 cd $PBS_O_WORKDIR
 
 source /home/lovelace/proj/proj832/fvperes/miniconda3/etc/profile.d/conda.sh
@@ -15,4 +19,4 @@ network=`ls -1 *_mcl.txt`
 out=${network/.txt/_betweenness.tsv}
 
 # columns = source node, target node, weight
-/usr/bin/time -v ./parallelBetweennessCentrality.py --edge_list $network --output $out --threads 128 --columns 0 1 2
+./parallelBetweennessCentrality.py --edge_list $network --output $out --threads 128 --columns 0 1 2
