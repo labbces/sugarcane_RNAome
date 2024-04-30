79f42b5dc2fde974b4af1593aad45ee112fe43b5
felipevzps <felipe.vzps@gmail.com>
2024-04-30 14:14:59 -0300

    running on par128

--- scripts/coExpression/MCL/run_mcl_CENAPAD.sh
+++ scripts/coExpression/MCL/run_mcl_CENAPAD.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #PBS -N MCL
-#PBS -q paralela
-#PBS -l nodes=4:ppn=128
+#PBS -q par128
+#PBS -l nodes=1:ppn=128
 #PBS -m abe
 #PBS -e run_mcl_cv1.2.sh.err
 #PBS -o run_mcl_cv1.2.sh.out
@@ -12,10 +12,10 @@ cd $PBS_O_WORKDIR
 source /home/lovelace/proj/proj832/fvperes/miniconda3/etc/profile.d/conda.sh
 conda activate MCL
 
-tasks=(1 2 3 4 5 6 7 8 9)
+tasks=(1 2 3 4 5 6 7 8)
 
 LC_NUMERIC="en_US.UTF-8"
-num=($(LC_NUMERIC="en_US.UTF-8" seq 1.8 0.5 6))
+num=($(LC_NUMERIC="en_US.UTF-8" seq 2.3 0.5 6))
 
 for task in "${tasks[@]}"; do
     export m=$((task-1))
@@ -24,6 +24,6 @@ for task in "${tasks[@]}"; do
     out=pearson_mcl_cv1.2/
 
     mkdir $out
-    mcl $in -I ${num[${m}]} -te $PBS_NUM_PPN --abc -analyze y -o ${out}out.${num[${m}]}
+    mcl $in -I ${num[${m}]} -te 128 -analyze y -o ${out}out.${num[${m}]} --abc
 
 done
