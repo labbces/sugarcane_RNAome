55534a7912ee87ee90414ff2e666b84b9f7135d5
felipevzps <felipe.vzps@gmail.com>
2024-05-01 15:30:09 -0300

    bash scrip to submit enrichment analysis only with significant values

---
+++ scripts/coExpression/moduleAnnotation/enrichmentGOsignificant.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#$ -q all.q
+#$ -V
+#$ -cwd
+#$ -t 1
+#$ -pe smp 1
+
+module load R/4.0.0
+
+/usr/bin/time -v Rscript enrichmentGO.R clusterSize.txt GO_annotations_BP_PPV0.6.tsv Hoang2017_counts_filters_VST_topCV_mcl_formated_cliques.csv
+/usr/bin/time -v Rscript enrichmentGO.R clusterSize.txt GO_annotations_BP_PPV0.6.tsv Correr2020_counts_filters_VST_topCV_mcl_formated_cliques.csv 
+/usr/bin/time -v Rscript enrichmentGO.R clusterSize.txt GO_annotations_BP_PPV0.6.tsv Perlo2022_counts_filters_VST_topCV_mcl_formated_cliques.csv 
