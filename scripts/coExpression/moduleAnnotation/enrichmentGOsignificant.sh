03cea43b0b77f75f1e9c39cb2fc404ccad4af75a
felipevzps <felipe.vzps@gmail.com>
2024-05-01 15:32:30 -0300

    fixing Rscript

--- scripts/coExpression/moduleAnnotation/enrichmentGOsignificant.sh
+++ scripts/coExpression/moduleAnnotation/enrichmentGOsignificant.sh
@@ -8,6 +8,6 @@
 
 module load R/4.0.0
 
-/usr/bin/time -v Rscript enrichmentGO.R clusterSize.txt GO_annotations_BP_PPV0.6.tsv Hoang2017_counts_filters_VST_topCV_mcl_formated_cliques.csv
-/usr/bin/time -v Rscript enrichmentGO.R clusterSize.txt GO_annotations_BP_PPV0.6.tsv Correr2020_counts_filters_VST_topCV_mcl_formated_cliques.csv 
-/usr/bin/time -v Rscript enrichmentGO.R clusterSize.txt GO_annotations_BP_PPV0.6.tsv Perlo2022_counts_filters_VST_topCV_mcl_formated_cliques.csv 
+/usr/bin/time -v Rscript enrichmentGOsignificant.R clusterSize.txt GO_annotations_BP_PPV0.6.tsv Hoang2017_counts_filters_VST_topCV_mcl_formated_cliques.csv
+/usr/bin/time -v Rscript enrichmentGOsignificant.R clusterSize.txt GO_annotations_BP_PPV0.6.tsv Correr2020_counts_filters_VST_topCV_mcl_formated_cliques.csv 
+/usr/bin/time -v Rscript enrichmentGOsignificant.R clusterSize.txt GO_annotations_BP_PPV0.6.tsv Perlo2022_counts_filters_VST_topCV_mcl_formated_cliques.csv 
