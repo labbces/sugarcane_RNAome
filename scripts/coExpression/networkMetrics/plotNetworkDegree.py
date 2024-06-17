a24ed1cadea23a2719c47c793878acabceb6dc30
Felipe Vaz Peres <felipe.vzps@gmail.com>
2024-06-17 18:42:15 -0300

    color degree by transcript function

--- scripts/coExpression/networkMetrics/plotNetworkDegree.py
+++ scripts/coExpression/networkMetrics/plotNetworkDegree.py
@@ -1,21 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import os 
 
-# Ler o arquivo com os dados dos graus dos nós
-file_path = "Perlo2022_counts_filters_VST_CNC_CV_above0.6_mcl_degree_sorted.tsv"
-data = pd.read_csv(file_path, delimiter="\t", header=None, names=["Node", "Degree"])
+file_paths_titles = [
+    ("Perlo2022_counts_filters_VST_CNC_CV_above0.6_mcl_degree_sorted_updated.tsv", "Degree distribution - Perlo2022 - CV > 0.6 - {bins} Bins"),
+    ("Correr2020_counts_filters_VST_CNC_CV_above_1.5_mcl_combined_degree_sorted_updated.tsv", "Degree distribution - Correr2020 - CV > 1.5 - {bins} Bins"),
+    ("Correr2020_counts_filters_VST_CNC_CV_above2_mcl_degree_sorted_updated.tsv", "Degree distribution - Correr2020 - CV > 2.0 - {bins} Bins"),
+    ("Hoang2017_counts_filters_VST_CNC_CV_above_1_mcl_degree_sorted_updated.tsv", "Degree distribution - Hoang2017 - CV > 1.0 - {bins} Bins"),
+    ("Hoang2017_counts_filters_VST_CNC_CV_above1.2_mcl_degree_sorted_updated.tsv", "Degree distribution - Hoang2017 - CV > 1.2 - {bins} Bins")
+]
 
-# Plotar a distribuição de links para os nós com diferentes números de bins
+colors = {'lncRNA': 'green', 'ncRNA': 'blue', 'protein-coding': 'red'}
+
 bin_sizes = [10, 100, 500, 1000]
-plt.figure(figsize=(15, 10))
 
-for i, bins in enumerate(bin_sizes, 1):
-    plt.subplot(2, 2, i)
-    plt.hist(data["Degree"], bins=bins, color='black', edgecolor='black')
-    plt.title(f"Degree distribution - {bins} Bins")
-    plt.xlabel("Degree")
-    plt.ylabel("Nodes")
-    plt.grid(True)
+for file_path, title_template in file_paths_titles:
+    data = pd.read_csv(file_path, delimiter="\t", header=None, names=["Gene", "Degree", "panRNAome classification", "Transcript", "Gene function", "Transcript size", "Transcript function"])
+    
+    plt.figure(figsize=(15, 10))
 
-plt.tight_layout()
-plt.show()
+    for i, bins in enumerate(bin_sizes, 1):
+        plt.subplot(2, 2, i)
+        
+        sns.histplot(data, x='Degree', hue='Transcript function', bins=bins, palette=colors, multiple='stack', edgecolor='black')
+        
+        plt.title(title_template.format(bins=bins))
+        plt.xlabel("Degree")
+        plt.ylabel("Nodes")
+        plt.grid(True)
+
+    plt.tight_layout()
+    #plt.show()
+
+    output_file = os.path.splitext(file_path)[0] + "_degree_distribution.png"
+    
+    plt.savefig(output_file)
+    plt.close()
