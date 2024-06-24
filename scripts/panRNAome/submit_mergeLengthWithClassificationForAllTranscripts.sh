0e103b8ccd8ab8e612638d810f4e2e4b83534bdf
felipevzps <felipe.vzps@gmail.com>
2024-06-24 07:26:49 -0300

    fixed merge length script

--- scripts/panRNAome/submit_mergeLengthWithClassificationForAllTranscripts.sh
+++ scripts/panRNAome/submit_mergeLengthWithClassificationForAllTranscripts.sh
@@ -6,4 +6,4 @@
 
 module load miniconda3
 
-/usr/bin/time -v ./mergeLengthWithClassificationForAllTranscripts.py -length 48_transcriptomes_length.tsv -classification panTranscriptome_panRNAomeClassificationTable_hyphen_Class_length.tsv
+/usr/bin/time -v ./mergeLengthWithClassificationForAllTranscripts.py -length 48_transcriptomes_length.tsv -classification panTranscriptome_panRNAomeClassificationTable_hyphen_Class_length.tsv -protein_coding proteinCoding_RNA.txt
