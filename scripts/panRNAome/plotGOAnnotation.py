c0e186e35b425e42dca4815f616170ccb1e0d02c
felipevzps <felipe.vzps@gmail.com>
2024-07-08 09:28:06 -0300

    update to plot transcripts with GO

--- scripts/panRNAome/plotGOAnnotation.py
+++ scripts/panRNAome/plotGOAnnotation.py
@@ -6,21 +6,23 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 annotation_file = 'updated_panTranscriptome_panRNAome_GeneFunction_Length_with_Rfam_with_GO.tsv'
-columns = ['panRNAome_category', 'gene_name', 'transcript_name', 'gene_function', 'transcript_length', 'transcript_function', 'Transcript Rfam family', 'Gene Rfam family', 'Transcript GO','Gene GO']
+columns = ['panRNAome_category', 'gene_name', 'transcript_name', 'gene_function', 'transcript_length', 'transcript_function', 'Transcript Rfam family', 'Transcript GO','Gene GO']
 annotation_df = pd.read_csv(annotation_file, sep='\t', header=None, names=columns)
 
 # nova coluna com presença ou ausencia de Gene Rfam family
 annotation_df['Has Gene GO'] = annotation_df['Gene GO'].notna()
+annotation_df['Has Transcript GO'] = annotation_df['Transcript GO'].notna()
 
 # agrupar por gene_name e gene_function e determinar se o gene possui uma "Gene Rfam family"
 gene_df = annotation_df.groupby(['gene_name', 'gene_function']).agg({'Has Gene GO': 'any'}).reset_index()
+transcript_df = annotation_df.groupby(['transcript_name', 'transcript_function']).agg({'Has Transcript GO': 'any'}).reset_index()
 
 # plot quantidade de genes em cada classificação usando a coluna 'gene_function'
 plt.figure(figsize=(10, 6))
 ax = sns.countplot(data=gene_df, x='gene_function')
-plt.title('Distribuição de Gene Ontology dos genes')
-plt.xlabel('Gene function')
-plt.ylabel('Frequência de genes')
+plt.title('Distribuição de grupos com Gene Ontology (BP)')
+plt.xlabel('Categoria dos grupos')
+plt.ylabel('Frequência dos grupos')
 plt.xticks(rotation=45)
 plt.tight_layout()
 
@@ -35,15 +37,15 @@ plt.savefig('gene_GO_distribution.png')
 plt.clf()  # Limpar a figura atual
 
 # filtrar genes com gene_function (lncRNA, protein and lncRNA, ncRNA, protein and non-coding)
-filtered_gene_df = gene_df.query('gene_function == "lncRNA" or gene_function == "protein and lncRNA" or gene_function == "ncRNA" or gene_function == "protein and non-coding"')
+#filtered_gene_df = gene_df.query('gene_function == "lncRNA" or gene_function == "ncRNA" or gene_function == "protein-coding" or gene_function == "protein and non-coding"')
 
-# plot quantidade de genes com e sem "Gene Rfam family"
+# plot quantidade de genes com e sem "Gene GO"
 plt.figure(figsize=(14, 8))
-ax = sns.countplot(data=filtered_gene_df, x='gene_function', hue='Has Gene GO')
-plt.title('Distribuição de ncRNAs com Gene Ontology')
-plt.xlabel('Gene function')
-plt.ylabel('Frequência de genes')
-plt.legend(title='Gene classificado com termo GO', bbox_to_anchor=(1.05, 1), loc='upper left')
+ax = sns.countplot(data=gene_df, x='gene_function', hue='Has Gene GO')
+plt.title('Distribuição de grupos com Gene Ontology (BP)')
+plt.xlabel('Categoria dos grupos')
+plt.ylabel('Frequência de grupos')
+plt.legend(title='Grupo classificado com termo GO', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.xticks(rotation=45)
 plt.tight_layout()
 
@@ -54,5 +56,25 @@ for p in ax.patches:
                 ha='center', va='bottom')
 
 plt.savefig('gene_function_by_GO_presence.png')
+#plt.show()
+plt.clf()  # Limpar a figura atual
+
+# plot quantidade de transcritos com e sem "Transcript GO"
+plt.figure(figsize=(14, 8))
+ax = sns.countplot(data=transcript_df, x='transcript_function', hue='Has Transcript GO')
+plt.title('Distribuição de transcritos com Gene Ontology (BP)')
+plt.xlabel('Categoria dos transcritos')
+plt.ylabel('Frequência de transcritos')
+plt.legend(title='Transcrito classificado com termo GO', bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# add label de contagem acima de cada barra
+for p in ax.patches:
+    ax.annotate(format(p.get_height(), '.0f'),
+                (p.get_x() + p.get_width() / 2., p.get_height()),
+                ha='center', va='bottom')
+
+plt.savefig('transcript_function_by_GO_presence.png')
 #plt.show()
 plt.clf()  # Limpar a figura atual
