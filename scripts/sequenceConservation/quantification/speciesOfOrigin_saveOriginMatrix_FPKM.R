126637704418234fd732236e69127f7456e99582
felipevzps <felipe.vzps@gmail.com>
2024-07-09 10:55:03 -0300

    function to transform counts to FPKM

--- scripts/sequenceConservation/quantification/speciesOfOrigin_saveOriginMatrix_FPKM.R
+++ scripts/sequenceConservation/quantification/speciesOfOrigin_saveOriginMatrix_FPKM.R
@@ -1,25 +1,16 @@
 library(tximport)
-library(reshape2)
-library(ggplot2)
-library(htmlwidgets)
-library(plotly)
-library(dplyr)
-library(cowplot)
 
 DIR="/Storage/data1/felipe.peres/Sugarcane_ncRNA/11_lncRNASequenceConservation/GenomicReads/PLOT"
 
 setwd(DIR)
 
 countsOrigin<-read.delim(file = "combined_matrix.sf",header=T,row.names=1)
-head(countsOrigin)
-colnames(countsOrigin)
-
 tx2gene <- read.table(file.path(DIR, "updated_panTranscriptome_panRNAome_GeneFunction_Length_with_Rfam_with_GO.tsv"), header = FALSE, sep = "\t")
 
-# teste merge
+# merge countsOrigin with annotation table
 countsOrigin <- merge(countsOrigin, tx2gene, by.x = "row.names", by.y = "V3")
-colnames(countsOrigin) <- c("Transcript","S._barberi","S._officinarum","S._spontaneum","S._bicolor", "Category", "Gene", "Function", "Transcript Size", "Transcript Function", "Transcript Rfam family", "Transcript GO", "Gene GO")
-#colnames(countsOrigin)<-c('S._barberi','S._officinarum','S._spontaneum')
+colnames(countsOrigin) <- c("Transcript","S._barberi","S._officinarum","S._spontaneum","S._bicolor", "EffectiveLength", "Category", "Gene", "Function", "Transcript Size", "Transcript Function", "Transcript Rfam family", "Transcript GO", "Gene GO")
+#colnames(countsOrigin)<-c('S._barberi','S._officinarum','S._spontaneum', 'S._bicolor', 'EffectiveLength')
 origNumberGenes=dim(countsOrigin)[1]
 
 dim(countsOrigin)
@@ -28,64 +19,81 @@ head(countsOrigin)
 genomes <- c("S._barberi","S._officinarum","S._spontaneum","S._bicolor")
 #table(rowSums(countsOrigin)==0)
 table(rowSums(countsOrigin[, genomes])==0)
-#countsOrigin<-countsOrigin[!rowSums(countsOrigin)==0,]               #remove transcript without reads mapped in both sps
-countsOrigin<-countsOrigin[!rowSums(countsOrigin[, genomes])==0,]     #remove transcript without reads mapped in both sps
+countsOrigin<-countsOrigin[!rowSums(countsOrigin[, genomes])==0,]     # remove transcript without reads mapped in both sps
 
-countsOrigin$CPM_SBAR<-((countsOrigin[,'S._barberi']+0.1)*10e6)/(sum(countsOrigin[,'S._barberi']))
-countsOrigin$CPM_SOFF<-((countsOrigin[,'S._officinarum']+0.1)*10e6)/(sum(countsOrigin[,'S._officinarum']))
-countsOrigin$CPM_SSPO<-((countsOrigin[,'S._spontaneum']+0.1)*10e6)/(sum(countsOrigin[,'S._spontaneum']))
-#countsOrigin$CPM_SBIC<-((countsOrigin[,'S._bicolor']+0.1)*10e6)/(sum(countsOrigin[,'S._bicolor']))
+# function from this blog post -> https://haroldpimentel.wordpress.com/2014/05/08/what-the-fpkm-a-review-rna-seq-expression-units/
+countsToFPKM <- function(counts, effLen)
+{
+  N <- sum(counts)
+  exp( log(counts) + log(1e9) - log(effLen) - log(N) )
+}
+
+counts_SBAR <- countsOrigin$S._barberi
+counts_SOFF <- countsOrigin$S._officinarum
+counts_SSPO <- countsOrigin$S._spontaneum
+counts_SBIC <- countsOrigin$S._bicolor
+effLen <- countsOrigin$EffectiveLength
+
+# apply countsToFPKM
+countsOrigin$FPKM_SBAR <- countsToFPKM(counts_SBAR, effLen)
+countsOrigin$FPKM_SOFF <- countsToFPKM(counts_SOFF, effLen)
+countsOrigin$FPKM_SSPO <- countsToFPKM(counts_SSPO, effLen)
+countsOrigin$FPKM_SBIC <- countsToFPKM(counts_SBIC, effLen)
 
 #ratio spontanenum vs officinarum - AB
-countsOrigin$ratioCPM_SSPO_vs_SOFF<-countsOrigin$CPM_SSPO/countsOrigin$CPM_SOFF
-countsOrigin$log10ratioCPM_SSPO_vs_SOFF<-round(log10(countsOrigin$CPM_SSPO/countsOrigin$CPM_SOFF),2)
+countsOrigin$ratioFPKM_SSPO_vs_SOFF <- countsOrigin$FPKM_SSPO/countsOrigin$FPKM_SOFF
+countsOrigin$log10ratioFPKM_SSPO_vs_SOFF <- round(log10(countsOrigin$FPKM_SSPO/countsOrigin$FPKM_SOFF),2)
 
 #ratio spontanenum vs barberi - AC
-countsOrigin$ratioCPM_SSPO_vs_SBAR<-countsOrigin$CPM_SSPO/countsOrigin$CPM_SBAR
-countsOrigin$log10ratioCPM_SSPO_vs_SBAR<-round(log10(countsOrigin$CPM_SSPO/countsOrigin$CPM_SBAR),2)
+countsOrigin$ratioFPKM_SSPO_vs_SBAR<-countsOrigin$FPKM_SSPO/countsOrigin$FPKM_SBAR
+countsOrigin$log10ratioFPKM_SSPO_vs_SBAR<-round(log10(countsOrigin$FPKM_SSPO/countsOrigin$FPKM_SBAR),2)
 
 #ratio spontanenum vs sorghum - AD
-#countsOrigin$ratioCPM_SSPO_vs_SBIC<-countsOrigin$CPM_SSPO/countsOrigin$CPM_SBIC
-#countsOrigin$log10ratioCPM_SSPO_vs_SBIC<-round(log10(countsOrigin$CPM_SSPO/countsOrigin$CPM_SBIC),2)
+#countsOrigin$ratioFPKM_SSPO_vs_SBIC<-countsOrigin$FPKM_SSPO/countsOrigin$FPKM_SBIC
+#countsOrigin$log10ratioFPKM_SSPO_vs_SBIC<-round(log10(countsOrigin$FPKM_SSPO/countsOrigin$FPKM_SBIC),2)
 
 # ratio officinarum vs barberi - BC
-countsOrigin$ratioCPM_SOFF_vs_SBAR<-countsOrigin$CPM_SOFF/countsOrigin$CPM_SBAR
-countsOrigin$log10ratioCPM_SOFF_vs_SBAR<-round(log10(countsOrigin$CPM_SOFF/countsOrigin$CPM_SBAR),2)
+countsOrigin$ratioFPKM_SOFF_vs_SBAR<-countsOrigin$FPKM_SOFF/countsOrigin$FPKM_SBAR
+countsOrigin$log10ratioFPKM_SOFF_vs_SBAR<-round(log10(countsOrigin$FPKM_SOFF/countsOrigin$FPKM_SBAR),2)
 
 #ratio officinarum vs sorghum - BD
-#countsOrigin$ratioCPM_SOFF_vs_SBIC<-countsOrigin$CPM_SOFF/countsOrigin$CPM_SBIC
-#countsOrigin$log10ratioCPM_SOFF_vs_SBIC<-round(log10(countsOrigin$CPM_SOFF/countsOrigin$CPM_SBIC),2)
+#countsOrigin$ratioFPKM_SOFF_vs_SBIC<-countsOrigin$FPKM_SOFF/countsOrigin$FPKM_SBIC
+#countsOrigin$log10ratioFPKM_SOFF_vs_SBIC<-round(log10(countsOrigin$FPKM_SOFF/countsOrigin$FPKM_SBIC),2)
 
 # ratio barberi vs sorghum - CD
-#countsOrigin$ratioCPM_SBAR_vs_SBIC<-countsOrigin$CPM_SBAR/countsOrigin$CPM_SBIC
-#countsOrigin$log10ratioCPM_SBAR_vs_SBIC<-round(log10(countsOrigin$CPM_SBAR/countsOrigin$CPM_SBIC),2)
+#countsOrigin$ratioFPKM_SBAR_vs_SBIC<-countsOrigin$FPKM_SBAR/countsOrigin$FPKM_SBIC
+#countsOrigin$log10ratioFPKM_SBAR_vs_SBIC<-round(log10(countsOrigin$FPKM_SBAR/countsOrigin$FPKM_SBIC),2)
 
-countsOrigin$Fraction_SSPO<-round(countsOrigin$CPM_SSPO/(countsOrigin$CPM_SOFF+countsOrigin$CPM_SSPO+countsOrigin$CPM_SBAR),3)
-countsOrigin$Fraction_SOFF<-round(countsOrigin$CPM_SOFF/(countsOrigin$CPM_SOFF+countsOrigin$CPM_SSPO+countsOrigin$CPM_SBAR),3)
-countsOrigin$Fraction_SBAR<-round(countsOrigin$CPM_SBAR/(countsOrigin$CPM_SOFF+countsOrigin$CPM_SSPO+countsOrigin$CPM_SBAR),3)
-#countsOrigin$Fraction_SBIC<-round(countsOrigin$CPM_SBIC/(countsOrigin$CPM_SOFF+countsOrigin$CPM_SSPO+countsOrigin$CPM_SBAR+countsOrigin$CPM_SBIC),3)
+countsOrigin$Fraction_SSPO<-round(countsOrigin$FPKM_SSPO/(countsOrigin$FPKM_SOFF+countsOrigin$FPKM_SSPO+countsOrigin$FPKM_SBAR),3)
+countsOrigin$Fraction_SOFF<-round(countsOrigin$FPKM_SOFF/(countsOrigin$FPKM_SOFF+countsOrigin$FPKM_SSPO+countsOrigin$FPKM_SBAR),3)
+countsOrigin$Fraction_SBAR<-round(countsOrigin$FPKM_SBAR/(countsOrigin$FPKM_SOFF+countsOrigin$FPKM_SSPO+countsOrigin$FPKM_SBAR),3)
+#countsOrigin$Fraction_SBIC<-round(countsOrigin$FPKM_SBIC/(countsOrigin$FPKM_SOFF+countsOrigin$FPKM_SSPO+countsOrigin$FPKM_SBAR+countsOrigin$FPKM_SBIC),3)
 
 head(countsOrigin)
 
 countsOrigin[which(countsOrigin$Fraction_SBAR >0.7),]
 countsOrigin$Origin<-NA
 
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & abs(countsOrigin$log10ratioCPM_SSPO_vs_SOFF) < 1 & abs(countsOrigin$log10ratioCPM_SOFF_vs_SBAR) < 1 & abs(countsOrigin$log10ratioCPM_SSPO_vs_SBAR) < 1),]),'Origin']<-'Common'
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$log10ratioCPM_SSPO_vs_SOFF >= 1 & abs(countsOrigin$log10ratioCPM_SOFF_vs_SBAR) < 1 & countsOrigin$log10ratioCPM_SSPO_vs_SBAR >= 1),]),'Origin']<-'SSPO'
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$log10ratioCPM_SSPO_vs_SOFF <= -1 & countsOrigin$log10ratioCPM_SOFF_vs_SBAR >= 1 & abs(countsOrigin$log10ratioCPM_SSPO_vs_SBAR) < 1),]),'Origin']<-'SOFF'
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & abs(countsOrigin$log10ratioCPM_SSPO_vs_SOFF) < 1 & countsOrigin$log10ratioCPM_SOFF_vs_SBAR <= -1 & countsOrigin$log10ratioCPM_SSPO_vs_SBAR <= -1),]),'Origin']<-'SBAR'
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._officinarum == 0 & countsOrigin$log10ratioCPM_SSPO_vs_SBAR <= -1),]),'Origin']<-'SBAR'
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._officinarum == 0 & countsOrigin$log10ratioCPM_SSPO_vs_SBAR >= 1),]),'Origin']<-'SSPO'
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._officinarum == 0 & abs(countsOrigin$log10ratioCPM_SSPO_vs_SBAR) < 1),]),'Origin']<-'CommonSSPO_SBAR'
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._spontaneum == 0 & countsOrigin$log10ratioCPM_SOFF_vs_SBAR <= -1),]),'Origin']<-'SBAR'
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._spontaneum == 0 & countsOrigin$log10ratioCPM_SOFF_vs_SBAR >= 1),]),'Origin']<-'SOFF'
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._spontaneum == 0 & abs(countsOrigin$log10ratioCPM_SOFF_vs_SBAR) < 1),]),'Origin']<-'CommonSOFF_SBAR'
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._barberi == 0 & countsOrigin$log10ratioCPM_SSPO_vs_SOFF <= -1),]),'Origin']<-'SOFF'
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._barberi == 0 & countsOrigin$log10ratioCPM_SSPO_vs_SOFF >= 1),]),'Origin']<-'SSPO'
-countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._barberi == 0 & abs(countsOrigin$log10ratioCPM_SSPO_vs_SOFF) <= 1),]),'Origin']<-'CommonSSPO_SOFF'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & abs(countsOrigin$log10ratioFPKM_SSPO_vs_SOFF) < 1 & abs(countsOrigin$log10ratioFPKM_SOFF_vs_SBAR) < 1 & abs(countsOrigin$log10ratioFPKM_SSPO_vs_SBAR) < 1),]),'Origin']<-'Common'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$log10ratioFPKM_SSPO_vs_SOFF >= 1 & abs(countsOrigin$log10ratioFPKM_SOFF_vs_SBAR) < 1 & countsOrigin$log10ratioFPKM_SSPO_vs_SBAR >= 1),]),'Origin']<-'SSPO'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$log10ratioFPKM_SSPO_vs_SOFF <= -1 & countsOrigin$log10ratioFPKM_SOFF_vs_SBAR >= 1 & abs(countsOrigin$log10ratioFPKM_SSPO_vs_SBAR) < 1),]),'Origin']<-'SOFF'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & abs(countsOrigin$log10ratioFPKM_SSPO_vs_SOFF) < 1 & countsOrigin$log10ratioFPKM_SOFF_vs_SBAR <= -1 & countsOrigin$log10ratioFPKM_SSPO_vs_SBAR <= -1),]),'Origin']<-'SBAR'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._officinarum == 0 & countsOrigin$log10ratioFPKM_SSPO_vs_SBAR <= -1),]),'Origin']<-'SBAR'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._officinarum == 0 & countsOrigin$log10ratioFPKM_SSPO_vs_SBAR >= 1),]),'Origin']<-'SSPO'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._officinarum == 0 & abs(countsOrigin$log10ratioFPKM_SSPO_vs_SBAR) < 1),]),'Origin']<-'CommonSSPO_SBAR'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._spontaneum == 0 & countsOrigin$log10ratioFPKM_SOFF_vs_SBAR <= -1),]),'Origin']<-'SBAR'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._spontaneum == 0 & countsOrigin$log10ratioFPKM_SOFF_vs_SBAR >= 1),]),'Origin']<-'SOFF'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._spontaneum == 0 & abs(countsOrigin$log10ratioFPKM_SOFF_vs_SBAR) < 1),]),'Origin']<-'CommonSOFF_SBAR'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._barberi == 0 & countsOrigin$log10ratioFPKM_SSPO_vs_SOFF <= -1),]),'Origin']<-'SOFF'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._barberi == 0 & countsOrigin$log10ratioFPKM_SSPO_vs_SOFF >= 1),]),'Origin']<-'SSPO'
+countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin) & countsOrigin$S._barberi == 0 & abs(countsOrigin$log10ratioFPKM_SSPO_vs_SOFF) <= 1),]),'Origin']<-'CommonSSPO_SOFF'
 countsOrigin[rownames(countsOrigin[which(is.na(countsOrigin$Origin)),]),'Origin']<-'UNK'
 
 table(countsOrigin$Origin, useNA = 'always')*100/sum(table(countsOrigin$Origin, useNA = 'always'))
 countsOrigin[which(is.na(countsOrigin$Origin)),]
 
-write.table(countsOrigin, sep = "\t", file = "speciesOfOriginPanTranscriptome.tsv", quote = FALSE, row.names = TRUE)
+# naming first column as "Name"
+countsOrigin$Name <- rownames(countsOrigin)
+countsOrigin <- countsOrigin[, c(ncol(countsOrigin), 1:(ncol(countsOrigin)-1))]
+
+write.table(countsOrigin, sep = "\t", file = "speciesOfOriginPanTranscriptome_FPKM.tsv", quote = FALSE, row.names = FALSE)
