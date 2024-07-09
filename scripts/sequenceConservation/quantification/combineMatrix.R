5087d3992d19dd60ba29bb16dc79df9238a68412
felipevzps <felipe.vzps@gmail.com>
2024-07-09 10:20:17 -0300

    combine matrix with EffectiveLength

--- scripts/sequenceConservation/quantification/combineMatrix.R
+++ scripts/sequenceConservation/quantification/combineMatrix.R
@@ -6,44 +6,32 @@ library(readr)
 DIR="/Storage/data1/felipe.peres/Sugarcane_ncRNA/11_lncRNASequenceConservation/GenomicReads/PLOT"
 
 setwd(DIR)
-#list.files(DIR)
 
-samples <- read.table(file.path(DIR, 'samples.txt'), header = TRUE, sep = '\t')
+samples <- read.table(file.path(DIR, "samples.txt"), header = TRUE, sep = '\t')
 files <- file.path(DIR, "../QUANT", samples$samples, "quant.sf")
 all(file.exists(files))
 
-# Read data with tximport (without tx2gene)
+# read data with tximport (using EffectiveLength instead of Length)
 txi <- tximport(files, type = "none", txIn = TRUE, txOut = TRUE, 
                 txIdCol = "Name", abundanceCol = "TPM", 
-                countsCol = "NumReads", lengthCol = "Length",
+                countsCol = "NumReads", lengthCol = "EffectiveLength",
                 importer = function(x) readr::read_tsv(x))
 
-# counts
-#head(txi$counts)
+sample_names <- samples$samples                   # extract sample names 
+colnames(txi$counts) <- sample_names              # add sample names to columns
 
-# abundance (TPM)
-#head(txi$abundance)
+counts <- txi$counts                              # extract counts
+genes <- rownames(counts)                         # extract gene names  
+effective_length <- rowMeans(txi$length)          # extract effective length means
 
-# Extract sample names from samples.txt
-sample_names <- samples$samples
-
-# Add names to columns
-colnames(txi$counts) <- sample_names
-
-# Extract counts and gene names
-counts <- txi$counts
-#head(counts)
-
-genes <- rownames(counts)
-
-# Combining new DataFrame
+# combining quant.sf files
 combined_matrix <- data.frame(
   Name = genes,
   S._barberi = rowSums(counts[, grepl("S._barberi", colnames(counts))]),
   S._officinarum = rowSums(counts[, grepl("S._officinarum", colnames(counts))]),
   S._spontaneum = rowSums(counts[, grepl("S._spontaneum", colnames(counts))]),
-  S._bicolor = rowSums(counts[, grepl("S._bicolor", colnames(counts))])
+  S._bicolor = rowSums(counts[, grepl("S._bicolor", colnames(counts))]),
+  EffectiveLength = effective_length
 )
 
-# Saving new DataFrame to combined_matrix.sf
 write.table(combined_matrix, file = "combined_matrix.sf", sep = "\t", quote = FALSE, row.names = FALSE)
