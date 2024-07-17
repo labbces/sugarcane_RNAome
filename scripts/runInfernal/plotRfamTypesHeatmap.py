492c504bbc70ab6c3c515e1a5d2e2f4df8c2953f
Felipe Vaz Peres <felipe.vzps@gmail.com>
2024-07-17 15:15:57 -0300

    reorder lncRNAs

--- scripts/runInfernal/plotRfamTypesHeatmap.py
+++ scripts/runInfernal/plotRfamTypesHeatmap.py
@@ -27,24 +27,40 @@ def extract_csv_gen_plot(directory_path):
 
     #print(df_norm_col)
 
-    labels = combined_df.T.values # labels for annotation
+    # Reorder df columns -  using transposed combined_df to keep numbers in order (lncRNAs on the left)
+    combined_df = combined_df.T[[' CD-box', ' leader', ' miRNA', ' riboswitch', ' tRNA', ' thermoregulator', '6S', 'Arthropod_7SK', 
+                               'Bacteria_small_SRP', 'EBv-sisRNA-2', 'Fungi_SRP', 'Intron', 'Metazoa_SRP', 'OLE', 'Plant_SRP', 
+                               'Protozoa_SRP', 'VA', 'enod40', 'tmRNA', ' lncRNA', ' rRNA', ' splicing', ' sRNA', ' ribozyme', 
+                               'Cis-reg', ' HACA-box', ' antisense']]
     
-    df_norm_col.index.names = ['Genotypes']
-    df_norm_col.columns.names = ['RNA family types']
+    labels = combined_df.values # labels for annotation
+    print(combined_df)
+
+    # Reorder df columns - now I am just keeping the new column order - from normalized df (lncRNAs on the left)
+    df_norm_col = df_norm_col[[' CD-box', ' leader', ' miRNA', ' riboswitch', ' tRNA', ' thermoregulator', '6S', 'Arthropod_7SK', 
+                               'Bacteria_small_SRP', 'EBv-sisRNA-2', 'Fungi_SRP', 'Intron', 'Metazoa_SRP', 'OLE', 'Plant_SRP', 
+                               'Protozoa_SRP', 'VA', 'enod40', 'tmRNA', ' lncRNA', ' rRNA', ' splicing', ' sRNA', ' ribozyme', 
+                               'Cis-reg', ' HACA-box', ' antisense']]
+    
+    df_norm_col.index.names = ['Genótipos']
+    df_norm_col.columns.names = ['Famílias de RNA']
+    
 
     plt.figure(figsize=(16, 12))
     g = sns.heatmap(df_norm_col, square=False, linewidth=0, xticklabels=True, yticklabels=True,
                     annot=labels, fmt='', annot_kws={"size": 8, "weight": "light"},
-                    cmap="YlGnBu", cbar_kws={'label': 'Normalized Count'})
+                    cmap="YlGnBu", cbar_kws={'label': 'Contagens normalizadas'})
 
     g.set_yticklabels(g.get_yticklabels(), rotation=0, fontsize=8)
     g.set_xticklabels(g.get_xticklabels(), rotation=70, fontsize=8)
-    g.set_title('Infernal Rfam family types')
+    g.set_title('Famílias de RNA do Rfam')
 
     plt.tight_layout()
-    #plt.show()
-    plt.savefig('panRNAomeRfamFamilies.png', dpi=300)#, bbox_inches='tight')
+    plt.show()
+    #plt.savefig('panRNAomeRfamFamilies.png', dpi=300)#, bbox_inches='tight')
 
 # Usage
-directory_path = '/home/felipe/Documents/sugarcane_RNAome/scripts/runInfernal/individualGenotypes' 
+directory_path = 'C:/Users/PC/Desktop/temp/Rfam/individualGenotypes' 
+#directory_path = '/home/felipe/Documents/sugarcane_RNAome/scripts/runInfernal/individualGenotypes' 
+
 extract_csv_gen_plot(directory_path)
