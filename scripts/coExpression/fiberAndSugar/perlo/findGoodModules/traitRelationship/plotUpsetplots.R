022ca4a3df4494eb6d02936a5ad2db221cdcae04
Felipe Vaz Peres <felipe.vzps@gmail.com>
2024-08-25 07:53:01 -0300

    update labels

--- scripts/coExpression/fiberAndSugar/perlo/findGoodModules/traitRelationship/plotUpsetplots.R
+++ scripts/coExpression/fiberAndSugar/perlo/findGoodModules/traitRelationship/plotUpsetplots.R
@@ -13,7 +13,9 @@ df2 <- read.table("week_bonferroni_005_rho_06.txt", header = T)
 df3 <- read.table("internode_bonferroni_005_rho_06.txt", header = T)
 df4 <- read.table("interaction_bonferroni_005_rho_06.txt", header = T)
 
-all <- list(genotype = df1$module, replicate = df2$module, internode = df3$module)#, interaction = df4$module)
-all_upset_plot <- ggVennDiagram(all, force_upset = T ,order.set.by = "none", nintersects = 3)
+all <- list("genótipo" = df1$module, "estágio de desenvolvimento" = df2$module, "entrenó" = df3$module)#, interaction = df4$module)
+all_upset_plot <- ggVennDiagram(all, force_upset = T ,order.set.by = "none", nintersects = 3,  top.bar.y.label = NULL,  sets.bar.x.label = "Módulos", intersection.matrix.color = "grey30")
 
-ggsave("upsetplot_correlations.png", all_upset_plot, "png", dpi = 300)
+
+#ggsave("upsetplot_correlations.png", all_upset_plot, "png", dpi = 300)
+
